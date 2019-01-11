7ed197bb384bb5057a517871fbcb0cc26680c347
AngNimaDawa <45613061+AngNimaDawa@users.noreply.github.com>
2019-01-11 08:51:15 +0545

    Create example.py

---
+++ example.py
@@ -0,0 +1,50 @@
+# dummy variablies
+# used for those variablies not used in the block of code
+# for _ in range(4):
+# 	print(_)
+
+
+
+# it get destoryed automatically itself
+
+# _, r, _ = 4,5,6
+# print(r)
+
+
+# _,*r,_ =  4,5,6,7
+# print(r)
+
+
+
+# #other useful context managers
+
+# asynchrous and synchrouns mode programming in python see self
+
+# gil = global interprated lock see your self
+# lock:
+#      acquire and release not permently
+# loads and dumps in jason simple
+# for file load and dum in jeson
+
+
+
+# v = ['d', 'h', 'm']
+# dict.fromkeys(v, 'all field can not be zero')
+
+
+
+
+# if any([0, 1, 0]):
+# 	error = dict.fromkeys(v, 'all field can not be zero') 
+# 	print(erro)
+
+
+
+
+
+# if any([0, 0, 0]):
+# 	error = dict.fromkeys(v, 'all field can not be zero') 
+# 	print(erro)
+
+
+# 	zen of python look your self
